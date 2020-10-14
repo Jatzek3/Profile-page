6527fbe2db3a2c2024f18ffc988f9c7a941eef1d
Jacek Kawalec <jokerchocolate@gmail.com>
2020-10-14 10:04:15 +0200

    routing setup

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.scss';
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from './Home/Home';
+import About from './About/About';
+import Contact from './Contact/Contact';
+import Navigation from './Navigation/Navigation'
 
 function App() {
   return (
     <BrowserRouter>
-      <Route path='/' component={Home} />
+      <Navigation />
+      <Switch>
+        <Route path='/about' component={About} />
+        <Route path='/contact' component={Contact} />
+        <Route path='/' component={Home} />
+      </Switch>
     </BrowserRouter>
   );
 }
