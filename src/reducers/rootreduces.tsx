7c4853a54ef51241d32b0de254e9a5a71059f694
Jacek Kawalec <jokerchocolate@gmail.com>
2020-10-15 18:16:23 +0200

    Added Readme

--- src/reducers/rootreduces.tsx
+++ src/reducers/rootreduces.tsx
@@ -2,11 +2,11 @@ import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 
 
-const initialState: object = {
-    posts: []
-}
 
 const rootReducer = combineReducers({
+    // 
+    // 
+    // 
     form: formReducer
 })
 export default rootReducer
